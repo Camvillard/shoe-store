e858a2458ac68d7064d8d97ba8a516c9d7e45242
camille villard <hello@camillevillard.com>
2022-06-26 16:17:09 -0400

    test(main): add first test to main component

--- front/src/components/main/Main.test.tsx
+++ front/src/components/main/Main.test.tsx
@@ -1,9 +1,10 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./Main.component";
+import Main from "./Main.component";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("should render a header and a footer", () => {
+  render(<Main />);
+  const header = screen.getByRole("banner");
+  const footer = screen.getByTestId("Main_footer");
+  expect(header).toBeInTheDocument();
+  expect(footer).toBeInTheDocument();
 });
