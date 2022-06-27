5ad98c571508e290484acb5a9c76fd5cced8f066
camille villard <hello@camillevillard.com>
2022-06-27 14:49:43 -0400

    feat(dashboard): add sales by store module

---
+++ front/src/components/sales/sales-by-store/sales-by-store.content.ts
@@ -0,0 +1,7 @@
+export const SALES_BY_STORE_CONTENT = {
+  lastUpdate: "last update:",
+  labelViewSales: "total sales",
+  labelViewInventory: "current stock",
+  switchLabelSales: "sales view",
+  switchLabelInventory: "inventory view",
+};
